ea89d9f8e564aa6dd960b90048edabeda7b9bb61
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-06-02 15:43:24 +0300

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""The Base Model Module"""
+"""Base Model Module"""
 
 
 from uuid import uuid4
@@ -7,34 +7,44 @@ from datetime import datetime
 
 
 class BaseModel:
-    """The BaseModel class"""
-
+    """
+    BaseModel class defines all common attributes/methods for other classes
+    """
     def __init__(self):
-        """Initialization method to set a unique id and save creation time"""
+        """
+        Initialization method defines the basic information
+        when an instance is created
+
+        Arguments:
+        id (str): A unique ID that will be given to each instance
+        created_at (datetime object) : the creation time of an instance
+        updated_at (datetime object) : the updating time of an instance
+        """
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        """A method returns information about the instance"""
-        return "[{}] ({}) {}".format
-        (self.__class__.__name__, self.id, self.__dict__)
+        """ A method the returns information about an object
+
+        Return:a string representation of the object
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
-        """A method to update updated_at to the current time"""
+        """ A method that updates the public instance attribute
+        updated_at with the current datetime """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        A method that returns a dictionary containing
-        all keys/values of __dict__ of the instance
+        """A method  returns a dictionary containing all
+           keys/values of __dict__ of the instance
 
-        Return:
-            info (dict): dictionary contains instance's information
-        """
-        info = self.__dict__.copy()
-        info["__class__"] = self.__class__.__name__
-        info["created_at"] = self.created_at.isoformat()
-        info["updated_at"] = self.updated_at.isoformat()
+        Return: a dictionary that contains object's information
 
-        return info
+        """
+        self.__dict__['__class__'] = self.__class__.__name__
+        self.__dict__['created_at'] = datetime.isoformat(self.created_at)
+        self.__dict__['updated_at'] = datetime.isoformat(self.updated_at)
+
+        return self.__dict__
